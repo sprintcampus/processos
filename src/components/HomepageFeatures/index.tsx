9bfffd1fac5ce46f1f2f01e489e31940b90588b4
710lucas <lucasperonicobarbotin@gmail.com>
2024-04-08 10:28:06 -0300

    adicionando modificações na página inicial do docusaurus

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -1,6 +1,7 @@
 import clsx from 'clsx';
 import Heading from '@theme/Heading';
 import styles from './styles.module.css';
+import "./styles.css"
 
 type FeatureItem = {
   title: string;
@@ -10,32 +11,11 @@ type FeatureItem = {
 
 const FeatureList: FeatureItem[] = [
   {
-    title: 'Easy to Use',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    title: 'Um projeto colaborativo',
+    Svg: require('@site/static/img/logo03.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
-      </>
-    ),
-  },
-  {
-    title: 'Focus on What Matters',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
-    description: (
-      <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
-      </>
-    ),
-  },
-  {
-    title: 'Powered by React',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
-    description: (
-      <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        Um projeto feito 100% pelos alunos da disciplina de Engenharia de software.
       </>
     ),
   },
@@ -57,14 +37,23 @@ function Feature({title, Svg, description}: FeatureItem) {
 
 export default function HomepageFeatures(): JSX.Element {
   return (
-    <section className={styles.features}>
-      <div className="container">
-        <div className="row">
-          {FeatureList.map((props, idx) => (
-            <Feature key={idx} {...props} />
-          ))}
-        </div>
-      </div>
-    </section>
+
+    <div className='features'>
+      {
+        FeatureList.map((props, index) => (
+          <Feature key={index} {...props} />
+        ))
+      }
+    </div>
+
+    // <section className={styles.features}>
+    //   <div className="container">
+    //     <div className="row">
+    //       {FeatureList.map((props, idx) => (
+    //         <Feature key={idx} {...props} />
+    //       ))}
+    //     </div>
+    //   </div>
+    // </section>
   );
 }
